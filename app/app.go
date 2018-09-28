e1a69d652a5ffc23baad7fa9a8afab4ecc00400d
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-09-28 19:10:15 +0200

    - first passing version - finish all code generation implementation

--- app/app.go
+++ app/app.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jalgoarena/problems-store/domain"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
@@ -17,10 +18,10 @@ import com.jalgoarena.type.*;
 
 public class Solution {
     /**
-{{ range .Function.Parameters }}    * @param {{ .Name }} {{ .Comment }}{{ end }}
-    * @return {{ .Function.Return.Comment }}
-    */
-    public void {{ .Function.Name }}() {
+{{ range .Function.Parameters }}     * @param {{ .Name }} {{ .Comment }}{{ end }}
+     * @return{{ .Function.Return.Comment }}
+     */
+    public {{ javaTypeDeclaration .Function.Return.Type .Function.Return.Generic }} {{ .Function.Name }}({{ methodParameters .Function.Parameters }}) {
         // Write your code here
     }
 }`
@@ -76,6 +77,10 @@ func GetSkeletonCode(c *gin.Context) {
 
 func buildSourceCode(problem *domain.Problem) (string, error) {
 	t := template.New("sourceCode")
+	t.Funcs(template.FuncMap{
+		"javaTypeDeclaration": javaTypeDeclaration,
+		"methodParameters":    methodParameters,
+	})
 	t, _ = t.Parse(tmpl)
 
 	buf := new(bytes.Buffer)
@@ -86,6 +91,69 @@ func buildSourceCode(problem *domain.Problem) (string, error) {
 	}
 
 	return buf.String(), nil
+}
+
+func methodParameters(parameters []domain.Parameter) string {
+	parametersAsStrings := mapParameters(&parameters, func(parameter domain.Parameter) string {
+		return fmt.Sprintf("%s %s", javaTypeDeclaration(parameter.Type, parameter.Generic), parameter.Name)
+	})
+
+	return strings.Join(parametersAsStrings, ", ")
+}
+
+func mapParameters(parameters *[]domain.Parameter, f func(domain.Parameter) string) []string {
+	result := make([]string, len(*parameters))
+	for i, v := range *parameters {
+		result[i] = f(v)
+	}
+	return result
+}
+
+func javaTypeDeclaration(returnType string, generic string) string {
+	switch returnType {
+	case "void":
+		return returnType
+	case "[I":
+		return "int[]"
+	case "[D":
+		return "double[]"
+	case "[[I":
+		return "int[][]"
+	case "[[C":
+		return "char[][]"
+	}
+
+	simpleType := simpleType(returnType)
+	return simpleOrGenericType(simpleType, generic)
+}
+
+func simpleOrGenericType(simpleType string, generic string) string {
+	switch simpleType {
+	case "Boolean":
+		return "bool"
+	case "Long":
+		return "long"
+	case "Integer":
+		return "int"
+	case "Double":
+		return "double"
+	default:
+		return withGeneric(simpleType, generic)
+	}
+}
+
+func simpleType(typeWithPackage string) string {
+	typeParts := strings.Split(typeWithPackage, ".")
+	return typeParts[len(typeParts)-1]
+}
+
+func withGeneric(baseType string, generic string) string {
+	switch generic {
+	case "":
+		return baseType
+	}
+
+	return fmt.Sprintf("%s<%s>", baseType, generic)
 }
 
 func loadProblem(problemsJson []byte) (*domain.Problem, error) {
